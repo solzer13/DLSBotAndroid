7504df11ad69097cb9e34f49c32a2d0b9567abb6
Solzer <solzer@yandex.ru>
2024-11-24 14:30:23 +0300

    Добавлен сбор ресурсов в регионе

--- app/src/main/java/xxx/solzer/dlsbot/modules/Farming.java
+++ app/src/main/java/xxx/solzer/dlsbot/modules/Farming.java
@@ -5,6 +5,8 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import xxx.solzer.dlsbot.App;
 import xxx.solzer.dlsbot.CommandService;
 import xxx.solzer.dlsbot.Module;
@@ -56,6 +58,22 @@ public class Farming extends Module {
     private static final double OIL_SEARCH_THRESHOLD = 0.9;
     private static final String OIL_SEARCH_NAME = "Поиск нефти";
 
+    private static final String TOMATOES_MAP_FILE = "map_tomatoes.png";
+    private static final double TOMATOES_MAP_THRESHOLD = 0.9;
+    private static final String TOMATOES_MAP_NAME = "Плитка помидор";
+
+    private static final String WOOD_MAP_FILE = "map_wood.png";
+    private static final double WOOD_MAP_THRESHOLD = 0.9;
+    private static final String WOOD_MAP_NAME = "Плитка дерева";
+
+    private static final String STEEL_MAP_FILE = "map_steel.png";
+    private static final double STEEL_MAP_THRESHOLD = 0.9;
+    private static final String STEEL_MAP_NAME = "Плитка стали";
+
+    private static final String OIL_MAP_FILE = "map_oil.png";
+    private static final double OIL_MAP_THRESHOLD = 0.9;
+    private static final String OIL_MAP_NAME = "Плитка нефти";
+
     private static final String COLLECT_FILE = "collect.png";
     private static final double COLLECT_THRESHOLD = 0.8;
     private static final String COLLECT_NAME = "Собрать";
@@ -83,6 +101,10 @@ public class Farming extends Module {
     private final Sprite btnSearchWood;
     private final Sprite btnSearchSteel;
     private final Sprite btnSearchOil;
+    private final Sprite btnMapTomatoes;
+    private final Sprite btnMaphWood;
+    private final Sprite btnMapSteel;
+    private final Sprite btnMapOil;
     private final Sprite btnCollect;
     private final Sprite btnArrow;
     private final Sprite btnCreateTroop;
@@ -93,6 +115,8 @@ public class Farming extends Module {
     private final boolean farmingWood;
     private final boolean farmingSteel;
     private final boolean farmingOil;
+
+    private long next_time;
     
     public Farming(){
         var prefs = App.getPreferences();
@@ -174,7 +198,35 @@ public class Farming extends Module {
                         Imgproc.TM_CCOEFF_NORMED,
                         OIL_SEARCH_THRESHOLD,
                         getPushMsgLog(OIL_SEARCH_NAME));
-        
+
+        btnMapTomatoes =
+                new Sprite(
+                        getAssetPath(TOMATOES_MAP_FILE),
+                        Imgproc.TM_CCOEFF_NORMED,
+                        TOMATOES_MAP_THRESHOLD,
+                        getPushMsgLog(TOMATOES_MAP_NAME));
+
+        btnMaphWood =
+                new Sprite(
+                        getAssetPath(WOOD_MAP_FILE),
+                        Imgproc.TM_CCOEFF_NORMED,
+                        WOOD_MAP_THRESHOLD,
+                        getPushMsgLog(WOOD_MAP_NAME));
+
+        btnMapSteel =
+                new Sprite(
+                        getAssetPath(STEEL_MAP_FILE),
+                        Imgproc.TM_CCOEFF_NORMED,
+                        STEEL_MAP_THRESHOLD,
+                        getPushMsgLog(STEEL_MAP_NAME));
+
+        btnMapOil =
+                new Sprite(
+                        getAssetPath(OIL_MAP_FILE),
+                        Imgproc.TM_CCOEFF_NORMED,
+                        OIL_MAP_THRESHOLD,
+                        getPushMsgLog(OIL_MAP_NAME));
+
         btnCollect =
                 new Sprite(
                         getAssetPath(COLLECT_FILE),
@@ -210,46 +262,93 @@ public class Farming extends Module {
         if(App.DEBUG){
             App.bus.post(new OnUserLog(TAG + ": Start"));
         }
-        
-        int busy = busy(mat);
 
-        if(farmingCount >= busy){
-            while(state.isRunning()){
-                if(farmingCount <= busy){
-                    break;
+        if(System.currentTimeMillis() > next_time){
+            int busy = busy(mat);
+
+            if(farmingCount >= busy){
+                while(state.isRunning()){
+                    if(farmingCount <= busy){
+                        break;
+                    }
+
+                    if(sendRandom(state)){
+                        next_time = System.currentTimeMillis() + 1000*60*3;
+                        break;
+                    }
+
+                    busy = busy(CommandService.takeScreenMat());
                 }
-                if(btnRegion.pushTimeout(state)) {
-                    if(btnSearchIcon.pushTimeout(state)){
-                        if(btnSearchTomatoes.pushTimeout(state)){
-                            if(btnSearch.pushTimeout(state)) {
-                                if(btnArrow.pushTimeout(state)) {
-                                    if(btnCollect.pushTimeout(state)) {
-                                        if(btnCreateTroop.pushTimeout(state)) {
-                                            btnMarch.pushTimeout(state);
-                                        }
-                                    }
+            }
+        }
+
+    }
+
+    private boolean sendRandom(CommandService.StateToken state){
+        while(state.isRunning()){
+            int rand = ThreadLocalRandom.current().nextInt(0, 3);
+            if(rand == 0 && farmingTomatoes){
+                return sendToResource(state, ResourceType.Tomatoes);
+            }
+            if(rand == 1 && farmingWood){
+                return sendToResource(state, ResourceType.Wood);
+            }
+            if(rand == 2 && farmingSteel){
+                return sendToResource(state, ResourceType.Steel);
+            }
+            if(rand == 3 && farmingOil){
+                return sendToResource(state, ResourceType.Oil);
+            }
+        }
+        return false;
+    }
+
+    private boolean sendToResource(CommandService.StateToken state, ResourceType type){
+        boolean result = false;
+        if(btnRegion.pushTimeout(state)) {
+            if(btnSearchIcon.pushTimeout(state)){
+                if(searchResource(state, type)){
+                    if(btnSearch.pushTimeout(state, 3000)) {
+                        App.bus.post(new OnTap(new Point((double) App.getScreenWidth() / 2, (double) App.getScreenHeight() / 2)));
+                        if(btnCollect.pushTimeout(state)) {
+                            if(btnCreateTroop.pushTimeout(state)) {
+                                if(btnMarch.pushTimeout(state)){
+                                    result = true;
                                 }
                             }
                         }
                     }
                 }
-                App.bus.post(new OnUserLog("Exit"));
-                while (state.isRunning()) {
-                    mat = CommandService.takeScreenMat();
+            }
+            goToHome(state);
+        }
+        return result;
+    }
 
-                    if(btnBack.pushIfExists(mat, 1000)) {
-                        continue;
-                    }
-                    if(btnHome.pushIfExists(mat, 1000)){
-                        continue;
-                    }
-                    if(btnRegion.isFound(mat)){
-                        break;
-                    }
-                }
-                busy = busy(CommandService.takeScreenMat());
+    private void goToHome(CommandService.StateToken state){
+        while (state.isRunning()) {
+            Mat mat = CommandService.takeScreenMat();
+
+            if(btnBack.pushIfExists(mat)) {
+                continue;
+            }
+            if(btnHome.pushIfExists(mat)){
+                continue;
+            }
+            if(btnRegion.isFound(mat)){
+                break;
             }
         }
+    }
+
+    private boolean searchResource(CommandService.StateToken state, ResourceType type){
+        switch (type){
+            case Tomatoes: return btnSearchTomatoes.pushTimeout(state);
+            case Wood: return btnSearchWood.pushTimeout(state);
+            case Steel: return btnSearchSteel.pushTimeout(state);
+            case Oil: return btnSearchOil.pushTimeout(state);
+        }
+        return false;
     }
     
     private int busy(Mat mat){
@@ -279,5 +378,9 @@ public class Farming extends Module {
 
     public String getTag() {
         return TAG;
+    }
+
+    private enum ResourceType {
+        Tomatoes, Wood, Steel, Oil;
     }
 }
